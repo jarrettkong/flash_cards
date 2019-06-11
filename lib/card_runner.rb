6837ad9dfc4d6728b69596cc063b8e41843e998c
Jarrett Kong <jarrettkong@me.com>
2019-06-11 12:47:27 -0600

    Add runner rb file

---
+++ lib/card_runner.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require './lib/card'
+require './lib/deck'
+require './lib/round'
+
+card_1 = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
+card_2 = Card.new('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', :STEM)
+card_3 = Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west', :STEM)
+
+deck = Deck.new([card_1, card_2, card_3])
+
+round = Round.new(deck)
+
+def start(deck, round)
+  index = 1
+  puts "Welcome! you're playing with #{deck.cards.length} card(s)!"
+  puts '------------------------------------------------'
+  while round.turns.length < deck.cards.length
+    puts "This is round #{index} of #{deck.cards.length}."
+    puts round.current_card.question
+    guess = gets.chomp
+    result = round.take_turn(guess)
+    puts result.feedback
+    index += 1
+  end
+
+  game_over(round, deck)
+end
+
+def game_over(round, deck)
+  categories = deck.cards.map(&:category).uniq
+  puts '***** Game Over! *****'
+  puts "You had #{round.number_correct} correct answers out of #{deck.cards.length} questions."
+  categories.each { |category| puts "#{category} - #{round.percent_correct_by_category(category)}% correct" }
+end
+
+start(deck, round)
--- lib/round.rb
+++ lib/round.rb
@@ -3,14 +3,13 @@
 require_relative './turn.rb'
 
 class Round
-  attr_accessor :deck, :turns, :current_card, :number_correct, :index
+  attr_accessor :deck, :turns, :current_card, :index
 
   def initialize(deck)
     @deck = deck
     @turns = []
     @index = 0
     @current_card = @deck.cards[@index]
-    @number_correct = 0
   end
 
   def take_turn(guess)
@@ -18,8 +17,12 @@ class Round
     turns.push(turn)
     self.index = index == deck.cards.length ? 0 : index + 1
     self.current_card = deck.cards[index]
-    self.number_correct += 1 if turn.correct?
     turn
+  end
+
+  def number_correct
+    correct = turns.select(&:correct?)
+    correct.length
   end
 
   def number_correct_by_category(category)
@@ -29,12 +32,12 @@ class Round
 
   def percent_correct
     correct = turns.select(&:correct?)
-    (correct.length.to_f / turns.length) * 100.0
+    ((correct.length.to_f / turns.length) * 100).to_i
   end
 
   def percent_correct_by_category(category)
     correct = turns.select { |turn| turn.correct? && turn.card.category == category }
     cards = deck.cards.select { |card| card.category == category }
-    (correct.length / cards.length) * 100.0
+    ((correct.length.to_f / cards.length) * 100).to_i
   end
 end
