879af91302e7a75f3cd9472ca2f3713f1467f089
Jarrett Kong <jarrettkong@me.com>
2019-06-10 19:05:10 -0600

    Add card initializer

--- lib/card.rb
+++ lib/card.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 class Card
-  attr_accessor :question, :answer, :category
+  attr_reader :question, :answer, :category
+
+  def initialize(question, answer, category)
+    @question = question
+    @answer = answer
+    @category = category
+  end
 end
